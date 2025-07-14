1a7dbc6ec3511cd54df72f7d839b891bdf09b659
wlwhstnqls <wlwhstnqls5@gmail.com>
2025-07-14 16:22:39 +0900

    메인작업스크립트

---
+++ sparat dungeon/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace sparat_dungeon
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("스파르타 던전 5조");
+        }
+    }
+}
